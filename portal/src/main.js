8b88611cafe8d8b78536e5ed30fafcd802ac8096
Your Name <you@example.com>
2022-02-18 01:47:48 +0800

    aa

--- portal/src/main.js
+++ portal/src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+
+
 const Counter = {
     data() {
       return {
@@ -15,4 +20,6 @@ const Counter = {
     }
   }
 
-createApp(App).mount('#app')//.use(ElementPlus)
+createApp(App).mount('#app').use(ElementPlus,{
+  locale: zhCn,
+})
