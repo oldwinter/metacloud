c56e4b91acfd8b07e8612f23f2f00e4dd1ea3c00
Your Name <you@example.com>
2022-02-21 18:51:26 +0800

    add cli oo

--- portal/src/main.ts
+++ portal/src/main.ts
@@ -7,3 +7,23 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+
+// import { registerMicroApps, start } from 'qiankun';
+
+// registerMicroApps([
+//   {
+//     name: 'react app', // app name registered
+//     entry: '//localhost:8080',
+//     container: '#yourContainer',
+//     activeRule: ()=>{return true},
+//   },
+//   {
+//     name: 'vue app',
+//     entry:'//localhost:8081',
+//     container: '#yourContainer2',
+//     activeRule: ()=>{return true},
+//   },
+// ]);
+
+// start();
