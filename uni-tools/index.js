98e8cc847e56d7a4f36f7ef819b71086f78b175b
oldwinter <cdd2zju@gmail.com>
2022-02-24 18:14:42 +0800

    调整uni-tools问题结构

--- uni-tools/index.js
+++ uni-tools/index.js
@@ -4,11 +4,17 @@ figlet.parseFont('Standard', standard);
 
 import prompts from 'prompts'
 import ora from 'ora';
+import { oraPromise } from 'ora';
 import chalk from 'chalk';
 
 import { exec, execSync } from 'child_process'
 import boxen from 'boxen';
 import clear from 'clear';
+import { resolve } from 'path';
+import { rejects } from 'assert';
+import util from 'util'
+
+const execP = util.promisify(exec);
 
 async function init() {
   async function banner() {
@@ -47,9 +53,10 @@ async function init() {
         message: 'dind(docker in docker) or dfromd(docker from docker)',
         choices: [
           { title: 'dind (recommended)', description: 'create a container including new docker and k8s', value: 'dind' },
-          { title: 'dfromd', description: 'create a container reuse your host resource like docker and k8s', value: 'dfromd', disabled: true }
+          { title: 'dfromd', description: 'create a container reuse your host resource like docker and k8s', value: 'dfromd' }
         ],
         initial: 0,
+
       },
       {
         type: "select",
@@ -80,99 +87,66 @@ async function init() {
     devRole
   } = result
   console.log(boxen(`${chalk.green("now:")} ${chalk.cyan(localOrRemote)}, run a ${chalk.cyan(dindOrDfromD)} env for ${chalk.cyan(devRole)}`, { padding: 1 }));
-  /*
-  ┌─────────────┐
-  │             │
-  │   unicorn   │
-  │             │
-  └─────────────┘
-  */
 
-  execSync('rm -rf metacloud')
+  const spin1 = ora('rm -rf metacloud').start()
+  await execP('rm -rf metacloud')
+  spin1.succeed()
+
   const spinnerCloneCode = ora('git clone ...').start()
-  let childGit = exec('git clone https://github.com/oldwinter/metacloud.git',
-    (error, stdout, stderr) => {
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
-    }
-  )
+  let childGit = await execP('git clone https://github.com/oldwinter/metacloud.git')
+  spinnerCloneCode.succeed('git clone done')
 
-  childGit.on('close', function (code) {
-    spinnerCloneCode.succeed('git clone done')
-    const spinnerNpmInstall = ora('npm install ...').start()
-    let childNpm = exec('npm install -g @vscode/dev-container-cli')
-    childNpm.on('close', function (code) {
-      spinnerNpmInstall.succeed('npm install done')
-      if (localOrRemote === 'locally' && dindOrDfromD === 'dind') {
-        // if (devRole === "frontend") {
-        //   // spinner.succeed('Done. Now run:\n')
-        //   // 等微软修复devcontainer的open功能
-        //   console.log(`${chalk.bold.green('cd metacloud/portal')}`)
-        //   console.log(`${chalk.bold.green('npm install')}`)
-        //   console.log(`${chalk.bold.green('npm run dev')}`)
-        // }
-        // if (devRole === "fullstack") {
-        //   console.log(`${chalk.bold.green('cd metacloud')}`)
-        //   console.log(`${chalk.bold.green('docker compose up')}`)
-        // }
-        // if (devRole === "devops") {
-        //   console.log(`${chalk.bold.green('open vscode')}`)
-        //   console.log(`${chalk.bold.green('open folder in container')}`)
-        // }
-        // 如果是dind模式，这里只build main devcontainer，其他container，需要在main里面build
-        const spinnerDockerBuild = ora('main devcontainer build ...').start()
-        let childBuild = exec('devcontainer build ./metacloud')
-        childBuild.on('close', function (code) {
-          spinnerDockerBuild.succeed('main devcontainer build done')
-        })
+  const spinnerNpmInstall = ora('npm install ...').start()
+  let childNpm = await execP('npm install -g @vscode/dev-container-cli')
+  spinnerNpmInstall.succeed('npm install done')
 
-      }
+   // 如果是dind模式，这里只build main devcontainer，其他container，需要在main里面build
+   const spinnerDockerBuild = ora('main devcontainer build ...').start()
+   let childBuild = await execP('devcontainer build ./metacloud --image-name meta-dev-admin:latest')
+   spinnerDockerBuild.succeed('main devcontainer build done')
 
-      if (localOrRemote === 'locally' && dindOrDfromD === 'dfromd') {
-        const spinnerDockerBuild = ora('main devcontainer build ...').start()
-        let childBuild = exec('devcontainer build ./metacloud')
-        childBuild.on('close', function (code) {
-          spinnerDockerBuild.succeed('main devcontainer build done')
-        })
-        const spinnerDockerBuild1 = ora('portal devcontainer build ...').start()
-        let childBuild1 = exec('devcontainer build ./metacloud/portal')
-        childBuild1.on('close', function (code) {
-          spinnerDockerBuild1.succeed('portal devcontainer build done')
-        })
-        const spinnerDockerBuild2 = ora('server-gin devcontainer build ...').start()
-        let childBuild2 = exec('devcontainer build ./metacloud/server/gin')
-        childBuild2.on('close', function (code) {
-          spinnerDockerBuild2.succeed('server-gin devcontainer build done')
-        })
-        const spinnerDockerBuild3 = ora('server-toy devcontainer build ...').start()
-        let childBuild3 = exec('devcontainer build ./metacloud/server/toy')
-        childBuild3.on('close', function (code) {
-          spinnerDockerBuild3.succeed('server-toy devcontainer build done')
-        })
+  if (localOrRemote === 'locally' && dindOrDfromD === 'dind') {
+
+  }
+
+  if (localOrRemote === 'locally' && dindOrDfromD === 'dfromd') {
+
+    const spinnerDockerBuild1 = ora('portal devcontainer build ...').start()
+    let childBuild1 = await execP('devcontainer build ./metacloud/portal --image-name meta-dev-portal:latest')
+    spinnerDockerBuild1.succeed('portal devcontainer build done')
+
+    const spinnerDockerBuild2 = ora('server-gin devcontainer build ...').start()
+    let childBuild2 = await execP('devcontainer build ./metacloud/server/gin --image-name meta-dev-server-gin:latest')
+    spinnerDockerBuild2.succeed('server-gin devcontainer build done')
+
+    const spinnerDockerBuild3 = ora('server-toy devcontainer build ...').start()
+    let childBuild3 = await execP('devcontainer build ./metacloud/server/toy --image-name meta-dev-server-toy:latest')
+    spinnerDockerBuild3.succeed('server-toy devcontainer build done')
 
 
-      }
+  }
 
-      if (devRole === "frontend") {
-        console.log(`${chalk.bold.green('cd metacloud/portal')}`)
-        console.log(`${chalk.bold.green('npm install')}`)
-        console.log(`${chalk.bold.green('npm run dev')}`)
-        console.log(`${chalk.bold.cyan('open browser')}`)
-      }
-      if (devRole === "fullstack") {
-        console.log(`${chalk.bold.green('cd metacloud')}`)
-        console.log(`${chalk.bold.green('docker compose up -f devops/docker-compose.yaml')}`)
-        console.log(`${chalk.bold.cyan('open vscode')}`)
-        console.log(`${chalk.bold.cyan('ctrl+shift+p: open folder in container, choose portal/ or server/gin/ or server/toy/')}`)
-      }
-      if (devRole === "devops") {
-        console.log(`${chalk.bold.cyan('open vscode')}`)
-        console.log(`${chalk.bold.cyan('ctrl+shift+p: open folder in container, choose metacloud/')}`)
-      }
+  if (devRole === "frontend") {
+    console.log(`${chalk.bold.green('go on with README.md chapter : 2.3')}`)
+    // console.log(`${chalk.bold.green('cd metacloud/portal')}`)
+    // console.log(`${chalk.bold.green('npm install')}`)
+    // console.log(`${chalk.bold.green('npm run dev')}`)
+    // console.log(`${chalk.bold.cyan('open browser')}`)
+  }
+  if (devRole === "fullstack") {
+    console.log(`${chalk.bold.green('go on with README.md chapter : 2.3')}`)
+    // console.log(`${chalk.bold.green('cd metacloud')}`)
+    // console.log(`${chalk.bold.green('docker compose up -f devops/docker-compose.yaml')}`)
+    // console.log(`${chalk.bold.cyan('open vscode')}`)
+    // console.log(`${chalk.bold.cyan('ctrl+shift+p: open folder in container, choose portal/ or server/gin/ or server/toy/')}`)
+  }
+  if (devRole === "devops") {
+    console.log(`${chalk.bold.green('go on with README.md chapter : 2.3')}`)
+    // console.log(`${chalk.bold.cyan('open vscode')}`)
+    // console.log(`${chalk.bold.cyan('ctrl+shift+p: open folder in container, choose metacloud/')}`)
+  }
 
-    })
-  });
+
 }
 
 init().catch((e) => {
